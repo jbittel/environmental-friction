052f48887d45eed434fc852f2fa724bf75369b26
Jason Bittel <jason.bittel@gmail.com>
2015-09-25 20:44:19 -0700

    Add current user as author when creating a post

--- environmental-friction/blog/admin.py
+++ environmental-friction/blog/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Post
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+        obj.save()
+
+
+admin.site.register(Post, PostAdmin)
