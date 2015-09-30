359976cfc9b506473ecc364602d2fe056660975d
Jason Bittel <jason.bittel@gmail.com>
2015-09-29 21:38:29 -0700

    Add more Post model and manager tests

--- environmental-friction/blog/tests/conftest.py
+++ environmental-friction/blog/tests/conftest.py
@@ -2,7 +2,9 @@ from pytest_factoryboy import register
 
 from .factories import AuthorFactory
 from .factories import PostFactory
+from .factories import UnpublishedPostFactory
 
 
 register(AuthorFactory)
 register(PostFactory)
+register(UnpublishedPostFactory)
--- environmental-friction/blog/tests/test_models.py
+++ environmental-friction/blog/tests/test_models.py
@@ -1,11 +1,29 @@
 from __future__ import unicode_literals
 
+import base32_crockford as b32
 import pytest
 
+from blog.models import Post
 from .factories import PostFactory
+from .factories import UnpublishedPostFactory
 
 
-@pytest.mark.django_db
+pytestmark = pytest.mark.django_db
+
+
 @pytest.mark.parametrize('post__title', ['First Post'])
 def test_post_slug(post):
     assert post.slug == 'first-post'
+
+
+@pytest.mark.parametrize('post__title', ['First Post'])
+def test_post_absolute_url(post):
+    assert post.get_absolute_url() == '/%s/first-post' % b32.encode(post.pk)
+
+
+def test_published_posts():
+    PostFactory()
+    UnpublishedPostFactory()
+
+    assert Post.objects.count() == 2
+    assert Post.published.count() == 1
