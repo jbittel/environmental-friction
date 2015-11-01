dc42c199b4dbd2953a204b794e60b8159eac9b01
Jason Bittel <jason.bittel@gmail.com>
2015-11-01 14:55:36 -0800

    Fix app dirs error with loaders specified

--- environmental-friction/config/settings/production.py
+++ environmental-friction/config/settings/production.py
@@ -3,6 +3,7 @@ from .base import *  # noqa
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
+TEMPLATES[0]['APP_DIRS'] = False
 TEMPLATES[0]['OPTIONS']['loaders'] = [
     ('django.template.loaders.cached.Loader', [
         'django.template.loaders.filesystem.Loader',
