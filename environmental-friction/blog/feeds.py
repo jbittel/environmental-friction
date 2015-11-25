505cb54ccd18c03c982847bdbc16d08e6cb726e9
Jason Bittel <jason.bittel@gmail.com>
2015-11-25 12:32:37 -0800

    Add Atom and RSS post feeds

---
+++ environmental-friction/blog/feeds.py
@@ -0,0 +1,28 @@
+from django.contrib.sites.models import Site
+from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse_lazy
+from django.utils.feedgenerator import Atom1Feed
+
+from .models import Post
+
+
+class RSSFeed(Feed):
+    title = Site.objects.get_current().name
+    link = reverse_lazy('blog:list')
+    feed_url = reverse_lazy('blog:rss')
+    description = 'Changing habits to transform communities'
+
+    def items(self):
+        return Post.objects.published()[:10]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.body
+
+
+class AtomFeed(RSSFeed):
+    feed_type = Atom1Feed
+    feed_url = reverse_lazy('blog:atom')
+    subtitle = RSSFeed.description
--- environmental-friction/blog/urls.py
+++ environmental-friction/blog/urls.py
@@ -1,11 +1,14 @@
-from django.conf.urls import patterns
 from django.conf.urls import url
 
+from .feeds import AtomFeed
+from .feeds import RSSFeed
 from .views import PostList
 from .views import PostDetail
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', PostList.as_view(), name='list'),
+    url(r'^feed/atom/$', AtomFeed(), name='atom'),
+    url(r'^feed/rss/$', RSSFeed(), name='rss'),
     url(r'^(?P<pk>\w+)(?:/(?P<slug>[\w\-]*))?$', PostDetail.as_view(), name='detail'),
-)
+]
