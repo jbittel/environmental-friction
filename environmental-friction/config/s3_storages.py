a44eadd3ef4dcc7647d30710f9ad87ac7ad08fd5
Jason Bittel <jason.bittel@gmail.com>
2015-11-27 22:03:28 -0800

    Use hashed files for production static files

--- environmental-friction/config/s3_storages.py
+++ environmental-friction/config/s3_storages.py
@@ -1,10 +1,12 @@
 from django.conf import settings
+from django.contrib.staticfiles.storage import ManifestFilesMixin
+
 from storages.backends.s3boto import S3BotoStorage
 
 
-class StaticStorage(S3BotoStorage):
+class StaticStorage(ManifestFilesMixin, S3BotoStorage):
     location = settings.STATICFILES_PATH
 
 
-class MediaStorage(S3BotoStorage):
+class MediaStorage(ManifestFilesMixin, S3BotoStorage):
     location = settings.MEDIAFILES_PATH
