1a1e99bc134942ad73113e7f1d680d9082c8e7c9
Jason Bittel <jason.bittel@gmail.com>
2015-12-14 19:49:44 -0800

    Add CSS post-processors

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,33 +12,35 @@ module.exports = function(grunt) {
     },
 
     sass: {
-      local: {
-        options: {
-          outputStyle: 'nested'
-        },
-        files: {
-          '<%= pkg.name %>/static/css/base.css': '<%= pkg.name %>/static/sass/base.scss'
-        }
-      },
-      production: {
-        options: {
-          outputStyle: 'compressed'
-        },
+      dist: {
         files: {
           '<%= pkg.name %>/static/css/base.css': '<%= pkg.name %>/static/sass/base.scss'
         }
       }
     },
 
+    postcss: {
+      options: {
+        processors: [
+          require('pixrem')(),
+          require('autoprefixer')(),
+          require('cssnano')()
+        ]
+      },
+      dist: {
+        src: '<%= pkg.name %>/static/css/*.css'
+      }
+    },
+
     watch: {
       sass: {
         files: '<%= pkg.name %>/static/sass/*.scss',
-        tasks: 'sass:local'
+        tasks: 'sass'
       }
     }
   });
 
-  grunt.registerTask('local', ['sass:local', 'bgShell', 'watch']);
-  grunt.registerTask('production', ['sass:production']);
+  grunt.registerTask('local', ['sass', 'bgShell', 'watch']);
+  grunt.registerTask('production', ['sass', 'postcss']);
   grunt.registerTask('default', ['production']);
 };
